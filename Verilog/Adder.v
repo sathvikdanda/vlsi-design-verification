21a18b8c11095276afae8d209c7ab87fcafacdca
Sathvik Sai Danda <sathvikdanda1221@gmail.com>
2026-08-13 19:47:40 +0530

    Implement 32-bit adder using two 16-bit adders

--- Verilog/Adder.v
+++ Verilog/Adder.v
@@ -1,1 +1,10 @@
+module top_module(
+    input [31:0] a,
+    input [31:0] b,
+    output [31:0] sum
+);
+    wire add16_out;
+    add16 ds1(.a(a[15:0]),.b(b[15:0]),.sum(sum[15:0]),.cout(add16_out));
+    add16 ds2(.a(a[31:16]),.b(b[31:16]),.cin(add16_out),.sum(sum[31:16]));
 
+endmodule

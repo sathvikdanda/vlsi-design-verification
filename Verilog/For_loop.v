d9b337e5ff2c3bda3ca1b49d8945e00a8d7ded32
Sathvik Sai Danda <sathvikdanda1221@gmail.com>
2026-08-15 20:12:04 +0530

    Implement for loop to reverse input array

--- Verilog/For_loop.v
+++ Verilog/For_loop.v
@@ -1,1 +1,11 @@
+module top_module( 
+    input [99:0] in,
+    output [99:0] out
+);
+    always @(*)
+        begin
+            for (int i=0;i<100;i++)
+                out [99-i]=in[i];
+        end
 
+endmodule
